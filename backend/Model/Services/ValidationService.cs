568bdd79d00493444b0ce43dc47566b47c0519da
Martin <marcingulbikci5@gmail.com>
2024-04-04 19:08:08 +0300

    Registration email validation implemented

--- backend/Model/Services/ValidationService.cs
+++ backend/Model/Services/ValidationService.cs
@@ -22,6 +22,13 @@ namespace Model.Services
             return new(true);
         }
 
+        public bool ValidateEmail(string email)
+        {
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(email);
+            return match.Success;
+        }
+
         public bool ValidatePhoneNumber(string phoneNumber)
         {
             Regex regex = new Regex(@"^(\+370|8)(6\d{7}|5\d{7}|46\d{6}|37\d{6}|45\d{6}|52\d{6}|4\d{7})$");
