046e95679e22fbcb7f7084bf52f9153417c8dfde
Martin <marcingulbikci5@gmail.com>
2024-04-04 18:46:13 +0300

    unit test for validating registration mandatory fields added

--- backend/Model/Repositories/ICarWorkshopRepository.cs
+++ backend/Model/Repositories/ICarWorkshopRepository.cs
@@ -5,8 +5,6 @@ namespace Model.Repositories
     public interface ICarWorkshopRepository
     {
         bool CheckIfCompanyNameExsits(string companyName);
-        CarWorkshopBasicData GetBasicByUsername(string username);
         void Insert(CarWorkshopExtendedData args);
-        bool ValidateCredentials(string username, string password);
     }
 }
---
+++ backend/Test/CarWorkshopAuthorizationTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.Entities.Authorization.Request;
+using Model.Entities.Authorization;
+using Model.Entities.CarService;
+using Model.Services;
+
+namespace Test
+{
+    public static class CarWorkshopAuthorizationData
+    {
+        public static readonly string username = Guid.NewGuid().ToString().Substring(0, 20);
+    }
+
+    [TestClass]
+    public class CarWorkshopAuthorizationTest
+    {
+        private readonly string password = "test-123";
+
+        private readonly ICarWorkshopService _carWorkshopService;
+
+        public CarWorkshopAuthorizationTest()
+        {
+            _carWorkshopService = ConfigurationMock.GetContainer().GetInstance<ICarWorkshopService>();
+        }
+
+        [TestMethod]
+        public void RegistrationMandatoryFieldsInvalid()
+        {
+            var registrationArgs = new RegistrationRequest<CarWorkshopRegistrationArgs>()
+            {
+                Username = "",
+                Password = "",
+                AdditionalData = new() { CompanyName = $"{CarWorkshopAuthorizationData.username}-company" },
+            };
+
+            var registrationResult = _carWorkshopService.Register(registrationArgs);
+
+            Assert.AreEqual(registrationResult.Data?.ResultCode, RegistrationResultCode.ValidationFailed);
+        }
+    }
+}
