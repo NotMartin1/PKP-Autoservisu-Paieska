5c9d2afc6b168a1ed8ba0c3a2ee0e6fc18f7fa14
Martin <marcingulbikci5@gmail.com>
2024-04-04 18:59:59 +0300

    Added unit test for registration phone number validation

---
+++ backend/Test/PhoneNumberGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public static class PhoneNumberGenerator
+{
+    private static readonly Random _random = new Random();
+
+    public static string GenerateLithuanianPhoneNumber()
+    {
+        string phoneNumber = "";
+
+        phoneNumber += "+370";
+
+        int nextDigit = _random.Next(2);
+
+        phoneNumber += nextDigit == 0 ? "6" : "5";
+
+        phoneNumber += GenerateDigits(7);
+
+        return phoneNumber;
+    }
+
+    private static string GenerateDigits(int count)
+    {
+        string digits = "";
+        for (int i = 0; i < count; i++)
+        {
+            digits += _random.Next(10);
+        }
+        return digits;
+    }
+}
