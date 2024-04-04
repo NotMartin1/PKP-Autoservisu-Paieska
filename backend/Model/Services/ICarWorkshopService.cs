329baa8b2a06faa874c99ab62def51b1eff29376
Martin <marcingulbikci5@gmail.com>
2024-04-04 19:17:19 +0300

    Added unit tests for checking invalid login credentials

--- backend/Model/Services/ICarWorkshopService.cs
+++ backend/Model/Services/ICarWorkshopService.cs
@@ -7,6 +7,7 @@ namespace Model.Services
 {
     public interface ICarWorkshopService
     {
+        ServiceResult<LoginResponse<CarWorkshopBasicData>> Login(LoginRequest request);
         ServiceResult<RegistrationResponse> Register(RegistrationRequest<CarWorkshopRegistrationArgs> request);
     }
 }
