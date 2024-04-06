2b05a4fda7f3acb034e5e4c97cb041271fda1d91
Martin <marcingulbikci5@gmail.com>
2024-04-06 16:15:03 +0300

    merge PAS-25-AutoservisuSarasas to master

--- backend/Model/obj/Debug/net6.0/Model.AssemblyInfo.cs
+++ backend/Model/obj/Debug/net6.0/Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76283a94278d1839b3726adf22c70e62a276c91f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
