d535a5c58c73b0432b268199f3cc4d8e4484651b
PanuprasB <pijus.blazys@gmail.com>
2024-04-15 19:51:55 +0300

    Added unit tests
    # Conflicts:
    #	backend/Model/obj/Debug/net6.0/Model.AssemblyInfo.cs
    #	backend/Model/obj/Debug/net6.0/Model.AssemblyInfoInputs.cache
    #	backend/Model/obj/Debug/net6.0/Model.assets.cache
    #	backend/Model/obj/Model.csproj.nuget.dgspec.json
    #	backend/Server.Core/obj/Debug/net6.0/Server.Core.AssemblyInfo.cs
    #	backend/Server.Core/obj/Debug/net6.0/Server.Core.AssemblyInfoInputs.cache
    #	backend/Server.Core/obj/Debug/net6.0/Server.Core.csproj.AssemblyReference.cache
    #	backend/Server.Core/obj/Server.Core.csproj.nuget.dgspec.json
    #	backend/Test/appsettings.json
    #	backend/WebApi/obj/Debug/net6.0/WebApi.AssemblyInfo.cs
    #	backend/WebApi/obj/Debug/net6.0/WebApi.AssemblyInfoInputs.cache
    #	backend/WebApi/obj/Debug/net6.0/WebApi.csproj.AssemblyReference.cache
    #	backend/WebApi/obj/WebApi.csproj.nuget.dgspec.json
    #	backend/WebApi/obj/project.assets.json

---
+++ backend/WebApi/Controllers/ConfigurationMock.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using SimpleInjector;
+
+namespace Test
+{
+    public static class ConfigurationMock
+    {
+        public static Container GetContainer()
+        {
+            Container container = new Container();
+            container.RegisterInstance(GetConfiguration());
+            Model.ObjectContainerInitializer.Init(container);
+            return container;
+        }
+
+        private static IConfiguration GetConfiguration()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            return configuration;
+        }
+    }
+}
