47ccdc73b98d3d8ff8e4a913eb9b09ba4d328fd0
Marcin <marcingulbicki5@gmail.com>
2024-04-16 09:52:42 +0300

    (ignore) missing commit added

--- backend/Server.Core/obj/Debug/net6.0/Server.Core.AssemblyInfo.cs
+++ backend/Server.Core/obj/Debug/net6.0/Server.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b27868cb83cfdbe5327e76b719c101be2114cf89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
