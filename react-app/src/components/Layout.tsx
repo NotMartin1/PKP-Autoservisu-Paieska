3a4ef5a70414940a33281bcc016af978d0c8cf17
Martin <marcingulbikci5@gmail.com>
2024-04-22 20:43:48 +0300

    (ignore) Remove unused definitions

--- react-app/src/components/Layout.tsx
+++ react-app/src/components/Layout.tsx
@@ -1,14 +1,6 @@
-import { Container } from 'reactstrap';
-import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
-
-import Sidebar from './Sidebar';
-import Header from './Header';
-
 const Layout = props => {
 
   const { children } = props;
-  const location = useLocation();
-  const isLoginPage = location.pathname === '/';
 
   return (
     <>
